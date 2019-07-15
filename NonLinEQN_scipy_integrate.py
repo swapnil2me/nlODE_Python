521be77260f6bf9ff4e69c67d5518d262f04a34c
swapnil2me <40165564+swapnil2me@users.noreply.github.com>
2019-07-15 12:29:43 +0530

    Updated file to include forward backward sweep

--- NonLinEQN_scipy_integrate.py
+++ NonLinEQN_scipy_integrate.py
@@ -19,32 +19,43 @@ def draw_fig(omgs, amp):
     #plt.plot(omgs, amp, 'ro')
     plt.show()
 
-F0 = 5
-omg = np.linspace(1.5, 4.5, 100)
-m = 1.0
-c = 0.0001
-k = 9.0
-a3=0.005
+F0 = 0.1e0
+omgf = np.linspace(2.5e0, 3.6e0, 500e0)
+omgb = np.flip(omgf)
+m = 1.0e0
+c = 0.01e0
+k = 9.0e0
+a3=0.005e2
 dataFold = 'my_data_fold'
 
 init = [0.0, 0.0]
-t = np.linspace(0.0, 10.0, 501)
+t = np.linspace(0.0, 500.0e0, 3001e0)
 plt.ion()
 plt.figure()
-for omgs in omg:
+for omgs in omgf:
     x = odeint(dampfunc, init, t,args=(F0, omgs,m,c,k,a3),mxordn=100)
     tp = 2*np.pi/omgs
     tmin = max(t)-10*tp
     amp = max(x[t>tmin,0])
+    init = [amp,0]
     f2 = plt.figure(1)
     plt.plot(omgs, amp, 'ro')
-    plt.axis([1.5,4.5,0,2])
-    plt.pause(0.001)
+    #plt.axis([1.5,10.5,0,25])
+    #plt.plot(t,x[:,0])
+    plt.pause(0.000000001)
     #plt.show()
     #drawnow(draw_fig(omgs, amp))
-#    plt.hold(True)
+    #plt.cla()
 #    plt.show()
 #save2Fold_timeseries_csv(x,'my_data','timeseries')
-
+for omgs in omgb:
+    x = odeint(dampfunc, init, t,args=(F0, omgs,m,c,k,a3),mxordn=100)
+    tp = 2*np.pi/omgs
+    tmin = max(t)-10*tp
+    amp = max(x[t>tmin,0])
+    init = [amp,0]
+    f2 = plt.figure(1)
+    plt.plot(omgs, amp, 'bo')
+    plt.pause(0.000000001)
 plt.show()
 #f2.show()
